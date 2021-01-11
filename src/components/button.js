5bf1b33122c53a2ebad7ed5dff33cc41c62a3dd3
amiRai <amirai0396@gmail.com>
2021-01-11 18:35:05 +0530

    navbar

--- src/components/button.js
+++ src/components/button.js
@@ -1,20 +1,35 @@
-import react from 'react';
+import React from 'react';
 import './button.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const STYLE = ['btn--primary' , 'btn--outline']
-const SIZE =['btn--medium' , 'btn--large'];
+const STYLES = ['btn--primary', 'btn--outline', 'btn--test'];
 
-export const Button = ({children,type,onClick,buttonStyle,buttonSize}) => {         
-    const checkButtonStyle =  STYLE.includes(buttonStyle) ? buttonStyle :STYLE[0] 
+const SIZES = ['btn--medium', 'btn--large'];
+
+export const Button = ({
+  children,
+  type,
+  onClick,
+  buttonStyle,
+  buttonSize
+}) => {
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
+
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+
   return (
-    <Link to ='/sign-up' className ='btn-mobile'>
-        <button 
-        className = {'btn ${checkButtonStyle} ${checkButtonSize}'} onClick = {onclick} type ={type}
-        >
-            {children}
-        </button>
-        
+    <Link to='/sign-up' className='btn-mobile'>
+      <button
+        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+        onClick={onClick}
+        type={type}
+      >
+        {children}
+      </button>
     </Link>
-    )
+  );
 };
+
+
